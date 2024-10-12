d46e2bbf316060798f230e5efb8f2729b5df02ab
CelestialLelouch <166859858+CelestialLelouch@users.noreply.github.com>
2024-10-12 17:22:06 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,85 @@
+function convertToBinaryString(value, bitLength){
+    let upperLimit = 2**bitLength - 1;
+    let lowerLimit = -(2**bitLength);
+
+    if(value > upperLimit || value < lowerLimit){
+        throw new Error('Input value is out of the range for this bit length.');
+    }else{
+        let binaryString = value.toString(2);
+        if(binaryString.length < bitLength){
+            binaryString = '0'.repeat(bitLength - binaryString.length) + binaryString;
+        }
+
+        return binaryString;
+    }
+}
+
+function addBinaryStrings(binValue_a, binValue_b){
+    let result="";
+    let carry = 0;
+    for(let i = binValue_a.length -1; i >= 0; i--){
+        value = Number(binValue_a[i]) + Number(binValue_b[i]);
+        if(value == 0 && carry == 1){
+            carry --;
+            value = 1;
+        }
+
+        if(value == 2){
+            carry ++;
+            value = 0;
+        }
+
+        result = value + result;
+    }
+
+    if(carry > 0){
+        for(let i = 0; i < carry; i++){
+            result = '1' + result;
+        }
+    }
+
+    return result;
+}
+
+function processExcess2m(value_a, value_b, bits){
+    const excess = 2**(bits-1)
+    calc_value_a = value_a + excess;
+    calc_value_b = value_b + excess;
+
+    total = value_a + value_b + excess;
+    if(total > 2**(bits) -1){
+        return total + ": Overflow occured "
+    }
+
+    let binary_a = convertToBinaryString(calc_value_a, bits);
+    let binary_b = convertToBinaryString(calc_value_b, bits);
+    let sum = addBinaryStrings(binary_a, binary_b);
+    document.getElementById('ord_bin').value = sum;
+    if (sum.length > bits){
+        sum = sum.slice(sum.length - bits);
+    }
+
+    if(sum[0] == "0"){
+        sum = sum.split("");
+        sum[0] = "1";
+    }else{
+        sum = sum.split("");
+        sum[0] = "0";
+    }
+
+    sum = sum.join("")
+    return sum;
+}
+
+
+function Add(){
+    let a = parseInt(document.getElementById("num1").value);
+    let b = parseInt(document.getElementById("num2").value);
+    let bits = parseInt(document.getElementById("bits").value);
+
+    document.getElementById("sum_num").value = a + b;
+    document.getElementById("bin_num1").value = convertToBinaryString(a + 2**(bits - 1), bits);
+    document.getElementById("bin_num2").value = convertToBinaryString(b + 2**(bits - 1), bits);
+    document.getElementById("sum_bin_exc").value = processExcess2m(a, b, bits); 
+    document.getElementById("exc_bin").value = processExcess2m(a, b, bits);
+}
